7eb1da1bb25e87c8f93e57998796735d07fec366
Tyler de Friess <tyler.defriess@gmail.com>
2020-01-13 21:08:53 -0500

    Stretch assignement timer complete

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,23 @@
+let tens = document.getElementById('secondTens');
+let ones = document.getElementById('secondOnes');
+let hundreds = document.getElementById('msHundreds');
+let msTens = document.getElementById('msTens');
+
+let counter = 0
+function myTimer(){    
+    counter++;
+    msTens.textContent = Math.floor((counter % 100)/10);
+    hundreds.textContent = counter % 10;
+    ones.textContent = Math.floor((counter % 1000)/100);
+    tens.textContent = Math.floor((counter % 10000) / 1000);    
+}
+
+var timer = window.setInterval(myTimer, 10);
+
+let digits = document.getElementsByClassName('digit');
+console.log(digits);
+setTimeout(() => {clearInterval(timer);
+    for (let i in digits){
+        digits[i].classList.add('redDigit');
+    }
+}, 10015);
